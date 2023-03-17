44e416212ef2a302f494d5b0f35963d60341c434
tqfx <tqfx@foxmail.com>
2023-03-17 23:50:31 +0800

    fix: meson: update sources

---
+++ script/meson.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os, re
+
+SUFFIXS = (".c", ".h", ".cc", ".hh", ".cpp", ".hpp", ".cxx", ".hxx")
+
+sources = []
+for dirpath, dirnames, filenames in os.walk("include"):
+    for filename in filenames:
+        source = os.path.join(dirpath, filename)
+        prefix, suffix = os.path.splitext(source)
+        if suffix in SUFFIXS:
+            sources.append(source)
+for dirpath, dirnames, filenames in os.walk("src"):
+    for filename in filenames:
+        source = os.path.join(dirpath, filename)
+        prefix, suffix = os.path.splitext(source)
+        if suffix in SUFFIXS:
+            sources.append(source)
+
+with open("meson.build", "r") as f:
+    meson = f.read()
+new = "[\n    '" + "',\n    '".join(sources) + "',\n]\n"
+old = re.findall(r"sources = ([^\]]+)", meson)[0] + "]\n"
+with open("meson.build", "wb") as f:
+    meson = f.write(meson.replace(old, new).encode())
