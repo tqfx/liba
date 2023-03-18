22dbafe0e6ccc63e185682a6d31bf5a22f9a02f8
tqfx <tqfx@foxmail.com>
2023-03-18 19:47:01 +0800

    fix: conan: bump to 2.0

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,15 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake
 
 
 class aConan(ConanFile):
     name = "liba"
     version = "0.1.1"
     license = "MPL-2.0"
+    topics = ("algorithm",)
     author = "tqfx tqfx@foxmail.com"
     homepage = url = "https://github.com/tqfx/liba.git"
     description = "An algorithm library based on C/C++ language"
-    topics = ("algorithm",)
-    generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [0, 1],
@@ -38,26 +38,19 @@ class aConan(ConanFile):
         if self.settings.os == "Windows":  # type: ignore
             del self.options.fPIC  # type: ignore
 
-    def source(self):
-        CMakeLists_txt = str(tools.load("CMakeLists.txt"))
-        if "conan_basic_setup" not in CMakeLists_txt:
-            CMakeLists_txt = CMakeLists_txt.replace(
-                "include(cmake/CMakeExportCompileCommands.cmake)\n",
-                "include(cmake/CMakeExportCompileCommands.cmake)\n"
-                "include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\n"
-                "conan_basic_setup()\n",
-            )
-        tools.save("CMakeLists.txt", CMakeLists_txt)
+    def generate(self):
+        cmake = CMakeToolchain(self)
+        cmake.variables["BUILD_TESTING"] = 0
+        cmake.variables["LIBA_SYMLINK"] = self.options.symlink  # type: ignore
+        cmake.variables["LIBA_PKGCONFIG"] = self.options.pkgconfig  # type: ignore
+        if self.settings.build_type != "Debug":  # type: ignore
+            cmake.variables["LIBA_LTO"] = self.options.lto  # type: ignore
+        else:
+            cmake.variables["LIBA_LTO"] = 0
+        cmake.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = 0
-        cmake.definitions["LIBA_SYMLINK"] = self.options.symlink  # type: ignore
-        cmake.definitions["LIBA_PKGCONFIG"] = self.options.pkgconfig  # type: ignore
-        if self.settings.build_type != "Debug":  # type: ignore
-            cmake.definitions["LIBA_LTO"] = self.options.lto  # type: ignore
-        else:
-            cmake.definitions["LIBA_LTO"] = 0
         cmake.configure()
         cmake.build()
 
