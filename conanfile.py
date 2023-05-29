b560949ee8ae4d57726fd663f141084b5094c531
tqfx <tqfx@foxmail.com>
2023-05-29 13:49:16 +0800

    opt: LTO=>IPO

--- conanfile.py
+++ conanfile.py
@@ -14,14 +14,14 @@ class aConan(ConanFile):
     options = {
         "shared": [0, 1],
         "fPIC": [0, 1],
-        "lto": [0, 1],
+        "ipo": [0, 1],
         "symlink": [0, 1],
         "pkgconfig": [0, 1],
     }
     default_options = {
         "shared": 0,
         "fPIC": 1,
-        "lto": 0,
+        "ipo": 0,
         "symlink": 0,
         "pkgconfig": 1,
     }
@@ -44,9 +44,9 @@ class aConan(ConanFile):
         cmake.variables["LIBA_SYMLINK"] = self.options.symlink  # type: ignore
         cmake.variables["LIBA_PKGCONFIG"] = self.options.pkgconfig  # type: ignore
         if self.settings.build_type != "Debug":  # type: ignore
-            cmake.variables["LIBA_LTO"] = self.options.lto  # type: ignore
+            cmake.variables["LIBA_IPO"] = self.options.ipo  # type: ignore
         else:
-            cmake.variables["LIBA_LTO"] = 0
+            cmake.variables["LIBA_IPO"] = 0
         cmake.generate()
 
     def build(self):
