127dd201978a1ec51e3c2205837d13f91e9f6dde
tqfx <tqfx@foxmail.com>
2023-07-23 22:37:49 +0800

    add: GCC system_header

--- java/src/a.h
+++ java/src/a.h
@@ -1,32 +1,17 @@
 #ifndef J_A_H
 #define J_A_H
 
+#if defined(__GNUC__) || defined(__clang__)
+#pragma GCC system_header
+#endif /* __GNUC__ */
 #include "a/a.h"
-#if defined(_MSC_VER)
-#pragma warning(disable : 4820)
-#endif /* _MSC_VER */
-#if A_PREREQ_GNUC(2, 95) || __has_warning("-Wpadded")
-#pragma GCC diagnostic ignored "-Wpadded"
-#endif /* -Wpadded */
 #if __has_warning("-Wreserved-id-macro")
 #pragma clang diagnostic ignored "-Wreserved-id-macro"
 #endif /* -Wreserved-id-macro */
 #if __has_warning("-Wreserved-identifier")
 #pragma clang diagnostic ignored "-Wreserved-identifier"
 #endif /* -Wreserved-identifier */
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic push
-#endif /* diagnostic */
-#if A_PREREQ_GNUC(6, 1) || __has_warning("-Wignored-attributes")
-#pragma GCC diagnostic ignored "-Wignored-attributes"
-#endif /* -Wignored-attributes */
-#if __has_warning("-Wlanguage-extension-token")
-#pragma clang diagnostic ignored "-Wlanguage-extension-token"
-#endif /* -Wlanguage-extension-token */
 #include <jni.h>
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic pop
-#endif /* diagnostic */
 
 #define CLASSPATH "liba/"
 #define JPACKAGE(F) Java_liba_##F
