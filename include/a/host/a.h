4a769c949702b0dde142b6755913e014f37ab6ff
tqfx <tqfx@foxmail.com>
2023-07-28 20:11:06 +0800

    doc: add comments for a_alloc?

--- include/a/host/a.h
+++ include/a/host/a.h
@@ -16,9 +16,21 @@
 extern "C" {
 #endif /* __cplusplus */
 
-A_EXTERN void *(*a_alloc)(void *, a_size_t);
-
+/*!
+ @brief default allocation function
+ @param[in] addr address of memory block
+ @param[in] size new size of memory block
+ @return new address of memory block or null
+*/
 A_EXTERN void *a_alloc_(void *addr, a_size_t size);
+
+/*!
+ @brief allocation function pointer
+ @param[in] addr address of memory block
+ @param[in] size new size of memory block
+ @return new address of memory block or null
+*/
+A_EXTERN void *(*a_alloc)(void *addr, a_size_t size);
 
 #if defined(__cplusplus)
 } /* extern "C" */
--- src/host/a.c
+++ src/host/a.c
@@ -1,7 +1,5 @@
 #include "a/host/a.h"
 
-void *(*a_alloc)(void *, a_size_t) = a_alloc_;
-
 void *a_alloc_(void *const addr, a_size_t const size)
 {
     if (size)
@@ -15,3 +13,5 @@ void *a_alloc_(void *const addr, a_size_t const size)
     free(addr);
     return A_NULL;
 }
+
+void *(*a_alloc)(void *, a_size_t) = a_alloc_;
