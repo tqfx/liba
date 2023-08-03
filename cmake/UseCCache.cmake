8a50a3e20d4755d94eb6ee19ca20046bd650da58
tqfx <tqfx@foxmail.com>
2023-08-03 21:24:11 +0800

    add: TemporaryDirectory.cmake

--- cmake/UseCCache.cmake
+++ cmake/UseCCache.cmake
@@ -4,7 +4,20 @@ if(CCACHE_FOUND)
   set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY RULE_LAUNCH_LINK "${CCACHE_EXECUTABLE}")
 endif()
 if(CCACHE_FOUND AND CMAKE_GENERATOR STREQUAL "Xcode")
-  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/cc
+  set(CMAKE_XCODE_ATTRIBUTE_CC ${CMAKE_CURRENT_BINARY_DIR}/cc)
+  set(CMAKE_XCODE_ATTRIBUTE_CXX ${CMAKE_CURRENT_BINARY_DIR}/cxx)
+  include(${CMAKE_CURRENT_LIST_DIR}/TargetSupportsSymbolicLink.cmake)
+  if(NOT TARGET_SUPPORTS_SYMBOLIC_LINK)
+    include(${CMAKE_CURRENT_LIST_DIR}/TemporaryDirectory.cmake)
+    if(EXISTS "${TEMPORARY_DIRECTORY}")
+      string(MD5 hash "${CMAKE_CURRENT_BINARY_DIR}")
+      set(CMAKE_XCODE_ATTRIBUTE_CC ${TEMPORARY_DIRECTORY}/cmake-${hash}/cc)
+      set(CMAKE_XCODE_ATTRIBUTE_CXX ${TEMPORARY_DIRECTORY}/cmake-${hash}/cxx)
+      set(hash)
+    endif()
+  endif()
+  set(CMAKE_XCODE_ATTRIBUTE_LD ${CMAKE_XCODE_ATTRIBUTE_CC})
+  file(WRITE ${CMAKE_XCODE_ATTRIBUTE_CC}
     "#!/usr/bin/env sh\n\n"
     "# Xcode generator doesn't include the compiler as the\n"
     "# first argument, Ninja and Makefiles do. Handle both cases.\n"
@@ -14,10 +27,9 @@ if(CCACHE_FOUND AND CMAKE_GENERATOR STREQUAL "Xcode")
     "export CCACHE_CPP2=true\n"
     "exec \"${CCACHE_EXECUTABLE}\" \"${CMAKE_C_COMPILER}\" \"$@\"\n"
   )
-  execute_process(COMMAND chmod a+rx ${CMAKE_CURRENT_BINARY_DIR}/cc)
-  set(CMAKE_XCODE_ATTRIBUTE_CC ${CMAKE_CURRENT_BINARY_DIR}/cc)
-  set(CMAKE_XCODE_ATTRIBUTE_LD ${CMAKE_CURRENT_BINARY_DIR}/cc)
-  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/cxx
+  execute_process(COMMAND chmod a+rx ${CMAKE_XCODE_ATTRIBUTE_CC})
+  set(CMAKE_XCODE_ATTRIBUTE_LDPLUSPLUS ${CMAKE_XCODE_ATTRIBUTE_CXX})
+  file(WRITE ${CMAKE_XCODE_ATTRIBUTE_CXX}
     "#!/usr/bin/env sh\n\n"
     "# Xcode generator doesn't include the compiler as the\n"
     "# first argument, Ninja and Makefiles do. Handle both cases.\n"
@@ -27,7 +39,5 @@ if(CCACHE_FOUND AND CMAKE_GENERATOR STREQUAL "Xcode")
     "export CCACHE_CPP2=true\n"
     "exec \"${CCACHE_EXECUTABLE}\" \"${CMAKE_CXX_COMPILER}\" \"$@\"\n"
   )
-  execute_process(COMMAND chmod a+rx ${CMAKE_CURRENT_BINARY_DIR}/cxx)
-  set(CMAKE_XCODE_ATTRIBUTE_CXX ${CMAKE_CURRENT_BINARY_DIR}/cxx)
-  set(CMAKE_XCODE_ATTRIBUTE_LDPLUSPLUS ${CMAKE_CURRENT_BINARY_DIR}/cxx)
+  execute_process(COMMAND chmod a+rx ${CMAKE_XCODE_ATTRIBUTE_CXX})
 endif()
