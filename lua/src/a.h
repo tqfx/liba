2eb8e492e800b8bb51b261df3c2fedee7657556f
tqfx <tqfx@tqfx.org>
2023-12-10 20:42:54 +0800

    fmt: sort function declarations prefixed with `luaopen_`

--- lua/src/a.h
+++ lua/src/a.h
@@ -127,15 +127,15 @@ void lua_stack_show(lua_State *L, unsigned int line);
 #define LUA_STACK(L) lua_stack_show(L, __LINE__)
 
 A_PUBLIC int luaopen_liba(lua_State *L);
+A_PUBLIC int luaopen_liba_complex(lua_State *L);
 A_PUBLIC int luaopen_liba_mf(lua_State *L);
-A_PUBLIC int luaopen_liba_tf(lua_State *L);
 A_PUBLIC int luaopen_liba_pid(lua_State *L);
 A_PUBLIC int luaopen_liba_pid_fuzzy(lua_State *L);
 A_PUBLIC int luaopen_liba_pid_neuro(lua_State *L);
-A_PUBLIC int luaopen_liba_complex(lua_State *L);
 A_PUBLIC int luaopen_liba_polytraj3(lua_State *L);
 A_PUBLIC int luaopen_liba_polytraj5(lua_State *L);
 A_PUBLIC int luaopen_liba_polytraj7(lua_State *L);
+A_PUBLIC int luaopen_liba_tf(lua_State *L);
 A_PUBLIC int luaopen_liba_version(lua_State *L);
 A_PUBLIC int liba_setter(lua_State *L);
 
