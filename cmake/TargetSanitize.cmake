e1bbc512755b63b9fbd75ea2471051fd10d20b55
tqfx <tqfx@tqfx.org>
2023-12-11 22:10:48 +0800

    remove string_append in TargetSanitize and TargetWarnings

--- cmake/TargetSanitize.cmake
+++ cmake/TargetSanitize.cmake
@@ -2,16 +2,6 @@ get_cmake_property(ENABLED_LANGUAGES ENABLED_LANGUAGES)
 include(CheckCXXCompilerFlag)
 include(CheckCCompilerFlag)
 
-function(string_append var)
-  foreach(arg ${ARGN})
-    string(FIND "${${var}}" "${arg}" index)
-    if(${index} EQUAL -1)
-      string(STRIP "${${var}} ${arg}" ${var})
-    endif()
-  endforeach()
-  set(${var} "${${var}}" PARENT_SCOPE)
-endfunction()
-
 function(list_append var)
   foreach(arg ${ARGN})
     list(FIND ${var} ${arg} index)
@@ -24,10 +14,8 @@ endfunction()
 
 macro(sanitize_flag)
   foreach(arg ${ARGN})
-    string_append(SANITIZE_C_FLAGS ${arg})
-    list_append(SANITIZE_CFLAGS ${arg})
-    string_append(SANITIZE_CXX_FLAGS ${arg})
-    list_append(SANITIZE_CXXFLAGS ${arg})
+    list_append(SANITIZE_CC ${arg})
+    list_append(SANITIZE_XX ${arg})
   endforeach()
 endmacro()
 
@@ -39,16 +27,14 @@ macro(sanitize_flag_cx)
     if(${index} GREATER -1)
       check_c_compiler_flag(${arg} cc${var})
       if(cc${var})
-        string_append(SANITIZE_C_FLAGS ${arg})
-        list_append(SANITIZE_CFLAGS ${arg})
+        list_append(SANITIZE_CC ${arg})
       endif()
     endif()
     list(FIND ENABLED_LANGUAGES CXX index)
     if(${index} GREATER -1)
-      check_cxx_compiler_flag(${arg} cxx${var})
-      if(cxx${var})
-        string_append(SANITIZE_CXX_FLAGS ${arg})
-        list_append(SANITIZE_CXXFLAGS ${arg})
+      check_cxx_compiler_flag(${arg} xx${var})
+      if(xx${var})
+        list_append(SANITIZE_XX ${arg})
       endif()
     endif()
     set(var)
@@ -56,7 +42,7 @@ macro(sanitize_flag_cx)
   set(index)
 endmacro()
 
-macro(warnings_flag_ld)
+macro(sanitize_flag_ld)
   foreach(arg ${ARGN})
     string(REPLACE "+" "x" var ${arg})
     string(REGEX REPLACE "[^A-Za-z0-9_-]+" "-" var ${var})
@@ -64,16 +50,14 @@ macro(warnings_flag_ld)
     if(${index} GREATER -1)
       check_c_compiler_flag(${arg} ld${var})
       if(ld${var})
-        string_append(SANITIZE_LD_FLAGS ${arg})
-        list_append(SANITIZE_LDFLAGS ${arg})
+        list_append(SANITIZE_LD ${arg})
       endif()
     endif()
     list(FIND ENABLED_LANGUAGES CXX index)
     if(${index} GREATER -1)
       check_cxx_compiler_flag(${arg} ld${var})
       if(ld${var})
-        string_append(SANITIZE_LD_FLAGS ${arg})
-        list_append(SANITIZE_LDFLAGS ${arg})
+        list_append(SANITIZE_LD ${arg})
       endif()
     endif()
     set(var)
@@ -102,14 +86,14 @@ if(
       CMAKE_C_COMPILER_ID MATCHES "GNU" OR
       CMAKE_CXX_COMPILER_ID MATCHES "GNU"
     )
-      warnings_flag_ld(-static-liblsan)
+      sanitize_flag_ld(-static-liblsan)
     endif()
   endif()
   if(
     CMAKE_C_COMPILER_ID MATCHES "GNU" OR
     CMAKE_CXX_COMPILER_ID MATCHES "GNU"
   )
-    warnings_flag_ld(-static-libubsan)
+    sanitize_flag_ld(-static-libubsan)
   endif()
   if(
     CMAKE_C_COMPILER_ID MATCHES "(Apple)?[Cc]lang" OR
@@ -117,7 +101,7 @@ if(
     CMAKE_CXX_COMPILER_ID MATCHES "(Apple)?[Cc]lang" OR
     CMAKE_CXX_COMPILER_ID MATCHES "IntelLLVM"
   )
-    warnings_flag_ld(-static-libsan)
+    sanitize_flag_ld(-static-libsan)
   endif()
 elseif(
   CMAKE_C_COMPILER_ID MATCHES "MSVC" OR
@@ -129,7 +113,7 @@ endif()
 function(target_compile_sanitize_3_3)
   foreach(target ${ARGN})
     if(TARGET ${target})
-      target_compile_options(${target} ${scope} ${SANITIZE_CFLAGS} ${SANITIZE_CXXFLAGS})
+      target_compile_options(${target} ${scope} ${SANITIZE_CC} ${SANITIZE_XX})
     endif()
   endforeach()
 endfunction()
@@ -146,8 +130,8 @@ function(target_compile_sanitize)
       continue()
     endif()
     target_compile_options(${target} ${scope}
-      $<$<COMPILE_LANGUAGE:C>:${SANITIZE_CFLAGS}>
-      $<$<COMPILE_LANGUAGE:CXX>:${SANITIZE_CXXFLAGS}>
+      $<$<COMPILE_LANGUAGE:C>:${SANITIZE_CC}>
+      $<$<COMPILE_LANGUAGE:CXX>:${SANITIZE_XX}>
     )
   endforeach()
 endfunction()
@@ -156,7 +140,7 @@ function(target_link_sanitize_3_13)
   foreach(target ${ARGN})
     if(TARGET ${target})
       get_property(LINK_FLAGS TARGET ${target} PROPERTY LINK_FLAGS)
-      string_append(LINK_FLAGS ${SANITIZE_CFLAGS} ${SANITIZE_CXXFLAGS} ${SANITIZE_LDFLAGS})
+      string_append(LINK_FLAGS ${SANITIZE_CC} ${SANITIZE_XX} ${SANITIZE_LD})
       set_property(TARGET ${target} PROPERTY LINK_FLAGS "${LINK_FLAGS}")
     endif()
   endforeach()
@@ -177,9 +161,9 @@ function(target_link_sanitize)
       continue()
     endif()
     target_link_options(${target} ${scope}
-      $<$<COMPILE_LANGUAGE:C>:${SANITIZE_CFLAGS}>
-      $<$<COMPILE_LANGUAGE:CXX>:${SANITIZE_CXXFLAGS}>
-      ${SANITIZE_LDFLAGS}
+      $<$<COMPILE_LANGUAGE:C>:${SANITIZE_CC}>
+      $<$<COMPILE_LANGUAGE:CXX>:${SANITIZE_XX}>
+      ${SANITIZE_LD}
     )
   endforeach()
 endfunction()
