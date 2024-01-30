20f46a0f44dd2b80ac9e6d65ab177195a759d279
tqfx <tqfx@tqfx.org>
2024-01-30 14:36:49 +0800

    update the test of lpf

--- test/lpf.h
+++ test/lpf.h
@@ -23,11 +23,12 @@ int MAIN(int argc, char *argv[]) // NOLINT(misc-definitions-in-headers)
     srand_(a_cast_s(unsigned long, time(A_NULL)));
     for (unsigned int i = 0; i < 100; ++i)
     {
-        a_float x = A_FLOAT_PI / 10 * a_float_c(i);
         a_float noise = (a_float_c(rand_()) / a_float_c(RAND_MAX_) - A_FLOAT_C(0.5));
-        a_float x1 = a_float_sin(x) + A_FLOAT_C(0.2) * noise;
+        a_float x = A_FLOAT_PI / 25 * a_float_c(i);
+        a_float x0 = a_float_sin(x);
+        a_float x1 = x0 + A_FLOAT_C(0.2) * noise;
         a_float x2 = a_lpf_iter(&ctx, x1);
-        debug(A_FLOAT_PRI("+", "f,") A_FLOAT_PRI("+", "f,") A_FLOAT_PRI("+", "f\n"), x, x1, x2);
+        debug(A_FLOAT_PRI("+", "f,") A_FLOAT_PRI("+", "f,") A_FLOAT_PRI("+", "f,") A_FLOAT_PRI("+", "f\n"), x, x0, x1, x2);
     }
     a_lpf_zero(&ctx);
 #if defined(__cplusplus) && (__cplusplus > 201100L)
