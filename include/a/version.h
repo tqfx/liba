63673590420ede0564e3809d08f7caed14712b30
tqfx <tqfx@tqfx.org>
2024-02-12 22:24:26 +0800

    update version tweak

--- include/a/version.h
+++ include/a/version.h
@@ -34,7 +34,7 @@
 
 /*! algorithm library version tweak */
 #ifndef A_VERSION_TWEAK
-#define A_VERSION_TWEAK 20240211
+#define A_VERSION_TWEAK 20240212
 #endif /* A_VERSION_TWEAK */
 
 #define A_VERSION_TOSTR(X) A_CAST_1(X)
