361747d4a8b01cbb2bafc5ec487dcbb267883174
tqfx <tqfx@tqfx.org>
2024-04-06 20:03:42 +0800

    support for luamake was removed

--- javascript/make.lua
+++
@@ -1,9 +0,0 @@
----@diagnostic disable
-local lm = require("luamake")
-lm.workdir = WORKDIR
-lm:executable("liba")({
-    defines = { "A_EXPORTS" },
-    includes = { "../include" },
-    sources = { "../src/*.c*", "src/*.c*" },
-    ldflags = { "--bind", "-sWASM_ASYNC_COMPILATION=0" },
-})
--- lua/make.lua
+++
@@ -1,9 +0,0 @@
----@diagnostic disable
-local lm = require("luamake")
-lm.workdir = WORKDIR
-lm:lua_library("liba")({
-    defines = { "A_EXPORTS" },
-    includes = { "../include" },
-    sources = { "../src/*.c*", "src/*.c*" },
-    links = lm.os ~= "windows" and { "m" } or {},
-})
