9b0884a2305cb8c0b567d0430250d142b259e574
tqfx <tqfx@tqfx.org>
2024-04-11 22:48:50 +0800

    add demo

---
+++ demo/main.c
@@ -0,0 +1,15 @@
+#include "a/version.h"
+#include "a/str.h"
+#include <stdio.h>
+int main(void)
+{
+    a_str *str = a_str_new();
+    a_str_putf(str, "%u.%u.%u+%u",
+               a_version_major,
+               a_version_minor,
+               a_version_patch,
+               a_version_tweak);
+    puts(a_str_ptr(str));
+    a_str_die(str);
+    return 0;
+}
