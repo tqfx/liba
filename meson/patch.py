87f16900a56521d47cd228f9a7c93b6778f72009
tqfx <tqfx@tqfx.org>
2024-05-01 16:55:02 +0800

    meson support for jni

--- meson/patch.py
+++ meson/patch.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
-import os, argparse
+import os, sys, argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("src")
 parser.add_argument("dst")
 args = parser.parse_known_args()
 
-if args[0].dst:
-    with open(args[0].dst, "r") as f:
-        text = f.read()
+with open(args[0].src, "r") as f:
+    text = f.read()
 
-    cur = "#if defined(A_HAVE_H)"
-    dir = os.path.dirname(args[0].src)
-    src = os.path.basename(args[0].src)
-    dst = os.path.basename(args[0].dst)
-    new = '#include "{}"\n{}'.format(src, cur)
-    text = text.replace(cur, new)
+cur = "#if defined(A_HAVE_H)"
+dir = os.path.dirname(args[0].dst)
+src = os.path.basename(args[0].src)
+dst = os.path.basename(args[0].dst)
+new = '#include "{}"\n{}'.format(dst, cur)
+res = text.replace(cur, new)
+new = os.path.join(dir, src)
 
-    with open(os.path.join(dir, dst), "wb") as f:
-        f.write(text.encode())
+if res != text:
+    with open(new, "wb") as f:
+        f.write(res.encode())
+
+dir = os.path.dirname(args[0].src)
+res = os.path.relpath(args[0].dst, dir)
+res = res.replace("\\", "/")
+sys.stdout.write(res)
