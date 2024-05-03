dc8e18d8dd86f7e392d1d19b40e1449b0084cd3a
tqfx <tqfx@tqfx.org>
2024-05-03 23:13:01 +0800

    add cargo features static_crt

--- build.rs
+++ build.rs
@@ -1,16 +1,15 @@
-use cmake::Config;
-
 fn main() {
-    let mut config = Config::new("");
+    let mut cmake = cmake::Config::new("");
 
-    config.define("BUILD_TESTING", "0");
+    cmake.define("BUILD_TESTING", "0");
     #[cfg(not(feature = "float"))]
-    config.define("LIBA_FLOAT", "8");
+    cmake.define("LIBA_FLOAT", "8");
     #[cfg(feature = "float")]
-    config.define("LIBA_FLOAT", "4");
-    config.static_crt(true);
+    cmake.define("LIBA_FLOAT", "4");
+    #[cfg(feature = "static_crt")]
+    cmake.static_crt(true);
 
-    let out = config.build();
+    let out = cmake.build();
     let lib = out.join("lib");
 
     println!("cargo:rustc-link-search=native={}", lib.display());
