2deb091edce4fd300677b736dd8c07cd368eb125
tqfx <tqfx@tqfx.org>
2024-05-09 19:22:00 +0800

    fix installation for apy in xmake

--- python/xmake.lua
+++ python/xmake.lua
@@ -1,9 +1,9 @@
 ---@diagnostic disable
 -- option: liba-python
 option("liba-python")
-set_default(false)
 set_showmenu(true)
 set_category("liba")
+set_values("python3", "python2")
 set_description("Enable/Disable Python")
 option_end()
 
@@ -11,25 +11,31 @@ rule("cython")
 set_extensions(".pyx")
 add_imports("lib.detect.find_tool")
 on_load(function(target)
-    local python = assert(find_tool("python"), "python not found!")
-    local so = os.iorunv(python.program, {
+    local python = get_config("liba-python")
+    local python = assert(find_tool(python) or find_tool("python"), "python not found!")
+    local suffix = os.iorunv(python.program, {
         "-c",
         'import sysconfig;print(sysconfig.get_config_var("EXT_SUFFIX") or sysconfig.get_config_var("SO"))',
     })
+    local prefix = os.iorunv(python.program, { "-c", 'import sysconfig;print(sysconfig.get_path("data"))' })
+    local platlib = os.iorunv(python.program, { "-c", 'import sysconfig;print(sysconfig.get_path("platlib"))' })
     target:set("kind", "shared")
     target:set("prefixname", "")
-    target:set("extension", so:trim())
+    target:set("extension", suffix:trim())
+    target:set("installdir", prefix:trim())
     target:add("runenvs", "PYTHONPATH", target:targetdir())
+    target:set("platlib", path.relative(platlib:trim(), prefix:trim()))
 end)
 on_buildcmd_file(function(target, batchcmds, sourcefile, opt)
-    local cython = assert(find_tool("cython") or find_tool("cython3"), "cython not found!")
-
-    batchcmds:show_progress(opt.progress, "${color.build.object}compiling.$(mode) %s", sourcefile)
-    batchcmds:vrunv(cython.program, { "--fast-fail", "--module-name", target:basename(), sourcefile })
-
+    local cython = assert(find_tool("cython3") or find_tool("cython"), "cython not found!")
     local sourcefile_c = sourcefile:replace(".pyx", ".c")
     local objectfile = target:objectfile(sourcefile_c)
     table.insert(target:objectfiles(), objectfile)
+
+    batchcmds:show_progress(opt.progress, "${color.build.object}compiling.cython %s", sourcefile)
+    batchcmds:vrunv(cython.program, { "--fast-fail", "--module-name", target:basename(), sourcefile })
+
+    batchcmds:show_progress(opt.progress, "${color.build.object}compiling.$(mode) %s", sourcefile_c)
     batchcmds:compile(sourcefile_c, objectfile)
 
     batchcmds:add_depfiles(sourcefile)
@@ -39,27 +45,27 @@ end)
 rule_end()
 
 if has_config("liba-python") then
-    add_requires("python")
+    add_requires(get_config("liba-python"))
     target("apy")
-    set_kind("shared")
     add_rules("cython")
     add_files("src/a.pyx")
     add_defines("A_EXPORTS")
-    add_imports("lib.detect.find_tool")
-    on_load(function(target)
-        target:set("targetdir", path.join(target:targetdir(), "python"))
-    end)
     on_install(function(target)
         local installdir = target:installdir()
         if installdir then
             print("installing %s to %s ..", target:name(), installdir)
-            installdir = path.join(installdir, "lib", "python")
+            installdir = path.join(installdir, target:get("platlib"))
             os.mkdir(installdir)
             os.vcp(target:targetfile(), installdir)
+            for _, installfile in ipairs(target:installfiles()) do
+                os.vcp(installfile, installdir)
+            end
         end
     end)
-    add_packages("python")
+    add_packages(get_config("liba-python"))
+    add_installfiles("liba.pyi")
     set_basename("liba")
+    set_targetdir(".")
     add_deps("a")
     target_end()
 end
