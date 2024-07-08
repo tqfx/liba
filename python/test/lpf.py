4455e880430f2c255022a6613475aaaf45b66f12
tqfx <tqfx@tqfx.org>
2024-07-08 20:40:28 +0800

    python/test: make print a function

--- python/test/lpf.py
+++ python/test/lpf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import os, sys, math, random
 
 path = os.path.dirname(__file__)
--- python/test/tf.py
+++ python/test/tf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import os, sys
 
 path = os.path.dirname(__file__)
