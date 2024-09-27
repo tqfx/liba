1f859facd3a3ac644ef038a833ea21f45cff8582
tqfx <tqfx@tqfx.org>
2024-09-27 22:54:52 +0800

    remove script/cargo.py

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 #[cfg(not(feature = "cmake"))]
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut make = cc::Build::new();
     make.include("include");
     make.define("A_EXPORTS", None);
@@ -7,34 +7,17 @@ fn main() {
     make.define("A_SIZE_FLOAT", "4");
     #[cfg(feature = "static_crt")]
     make.static_crt(true);
-    make.file("src/a.c")
-        .file("src/avl.c")
-        .file("src/buf.c")
-        .file("src/complex.c")
-        .file("src/crc.c")
-        .file("src/fuzzy.c")
-        .file("src/math.c")
-        .file("src/mf.c")
-        .file("src/pid.c")
-        .file("src/pid_fuzzy.c")
-        .file("src/pid_neuro.c")
-        .file("src/poly.c")
-        .file("src/que.c")
-        .file("src/rbt.c")
-        .file("src/regress.c")
-        .file("src/regress_linear.c")
-        .file("src/regress_simple.c")
-        .file("src/str.c")
-        .file("src/tf.c")
-        .file("src/trajbell.c")
-        .file("src/trajpoly3.c")
-        .file("src/trajpoly5.c")
-        .file("src/trajpoly7.c")
-        .file("src/trajtrap.c")
-        .file("src/utf.c")
-        .file("src/vec.c")
-        .file("src/version.c")
-        .compile("a");
+
+    for entry in std::fs::read_dir("src")? {
+        let path = entry?.path();
+        let extension = path.extension().unwrap();
+        if path.is_file() & extension.eq("c") {
+            make.file(path);
+        }
+    }
+
+    make.compile("a");
+    Ok(())
 }
 #[cfg(feature = "cmake")]
 fn main() {
