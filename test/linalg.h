4537345996dae8b1460da281c2010729bd504392
tqfx <tqfx@tqfx.org>
2024-12-17 08:38:57 +0800

    compile with c90

--- test/linalg.h
+++ test/linalg.h
@@ -4,19 +4,20 @@
 
 static void test_mul(void)
 {
+    a_uint i;
     a_float z[3 * 3];
     a_float x[3 * 3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     a_float y[3 * 3] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 
     debug("X\t={");
-    for (int i = 0; i < 9; ++i)
+    for (i = 0; i < 9; ++i)
     {
         debug("%" A_FLOAT_PRI "g%c", x[i], i < 8 ? ',' : '}');
     }
     debug("\n");
 
     debug("Y\t={");
-    for (int i = 0; i < 9; ++i)
+    for (i = 0; i < 9; ++i)
     {
         debug("%" A_FLOAT_PRI "g%c", y[i], i < 8 ? ',' : '}');
     }
@@ -25,7 +26,7 @@ static void test_mul(void)
     a_linalg_mulmm(z, x, y, 3, 3, 3);
 
     debug("XY\t={");
-    for (int i = 0; i < 9; ++i)
+    for (i = 0; i < 9; ++i)
     {
         debug("%" A_FLOAT_PRI "g%c", z[i], i < 8 ? ',' : '}');
     }
@@ -34,7 +35,7 @@ static void test_mul(void)
     a_linalg_mulTm(z, x, y, 3, 3, 3);
 
     debug("X'Y\t={");
-    for (int i = 0; i < 9; ++i)
+    for (i = 0; i < 9; ++i)
     {
         debug("%" A_FLOAT_PRI "g%c", z[i], i < 8 ? ',' : '}');
     }
@@ -43,7 +44,7 @@ static void test_mul(void)
     a_linalg_mulmT(z, x, y, 3, 3, 3);
 
     debug("XY'\t={");
-    for (int i = 0; i < 9; ++i)
+    for (i = 0; i < 9; ++i)
     {
         debug("%" A_FLOAT_PRI "g%c", z[i], i < 8 ? ',' : '}');
     }
@@ -52,7 +53,7 @@ static void test_mul(void)
     a_linalg_mulTT(z, x, y, 3, 3, 3);
 
     debug("X'Y'\t={");
-    for (int i = 0; i < 9; ++i)
+    for (i = 0; i < 9; ++i)
     {
         debug("%" A_FLOAT_PRI "g%c", z[i], i < 8 ? ',' : '}');
     }
