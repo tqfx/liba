7a1be3c8e6c6929c54a2968ddd5110937bbb8e1f
tqfx <tqfx@tqfx.org>
2025-01-02 18:49:42 +0800

    bump copyright year to 2025

--- include/a/version.h
+++ include/a/version.h
@@ -34,7 +34,7 @@
 
 /*! algorithm library version tweak */
 #ifndef A_VERSION_TWEAK
-#define A_VERSION_TWEAK 20240930
+#define A_VERSION_TWEAK 20250101
 #endif /* A_VERSION_TWEAK */
 
 #define A_VERSION_TOSTR(X) A_CAST_1(X)
